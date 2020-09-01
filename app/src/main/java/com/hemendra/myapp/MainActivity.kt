ff656fcda58a1761f156998630bf8eb3f971a417
hemu <hemendraoid>
2020-09-01 08:20:11 +0530

    initial commit

--- app/src/main/java/com/hemendra/myapp/MainActivity.kt
+++ app/src/main/java/com/hemendra/myapp/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.hemendra.myapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("create","oncreate is called")
+        login_btn.setOnClickListener {
+
+            var intent = Intent(this@MainActivity,SecondActivity::class.java)
+            startActivity(intent)
+
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("start","onstart is called")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("resume","onresume is called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("pause","onpause is called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("stop","onstop is called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Restart","onRestart is called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Destroy","onDestroy is called")
+    }
+
 }
--- app/src/main/java/com/hemendra/myapp/SecondActivity.java
+++ app/src/main/java/com/hemendra/myapp/SecondActivity.java
@@ -3,6 +3,7 @@ package com.hemendra.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Log.i("Create-Activity2","onCreate is called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Start-Activity2","onStart is called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Resume-Activity2","onResume is called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Pause-Activity2","onPause is called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Stop-Activity2","onStop is called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Restart-Activity2","onRestart is called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Destroy-Activity2","onDestroy is called");
     }
 }
